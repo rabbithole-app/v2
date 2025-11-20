9a98bf75f0167cd69d1832844fc062246bf8804f
Khalik Faradzhli <khalik.faradzhli@mail.ru>
2025-11-20 12:36:18 +0300

    feat: add shared library for common components
    Create non-buildable shared library for rabbithole and storage apps

---
+++ libs/shared/src/index.ts
@@ -0,0 +1,4 @@
+// Export shared components, services, and utilities here
+// This library is non-buildable and will be included directly in applications
+
+export * from './lib/shared/shared.component';
