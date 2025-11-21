d4d8a3ce5404a7d869141339f620792412f2dfa7
Khalik Faradzhli <khalik.faradzhli@mail.ru>
2025-11-21 13:31:41 +0300

    chore: update dependencies and export new components

--- libs/shared/src/index.ts
+++ libs/shared/src/index.ts
@@ -1,4 +1,8 @@
 // Export shared components, services, and utilities here
 // This library is non-buildable and will be included directly in applications
 
+export * from './lib/components/canister/canister-controllers-table/canister-controllers-table.component';
+export * from './lib/components/canister/canister-memory-chart/canister-memory-chart.component';
+export * from './lib/components/canister/canister-memory/canister-memory.component';
+export * from './lib/components/canister/inline-warning/inline-warning.component';
 export * from './lib/shared/shared.component';
