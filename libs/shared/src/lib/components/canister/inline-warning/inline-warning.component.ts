4e60e46e5048303facb2c40f44764d9a7b874c36
Khalik Faradzhli <khalik.faradzhli@mail.ru>
2025-11-21 13:31:15 +0300

    feat(shared): add canister memory display components
    - Add canister memory component with chart
    - Add inline warning component
    - Display memory usage and warnings

---
+++ libs/shared/src/lib/components/canister/inline-warning/inline-warning.component.ts
@@ -0,0 +1,32 @@
+import {
+  ChangeDetectionStrategy,
+  Component,
+  input,
+} from '@angular/core';
+import { NgIcon, provideIcons } from '@ng-icons/core';
+import { lucideAlertTriangle } from '@ng-icons/lucide';
+import { HlmIcon } from '@spartan-ng/helm/icon';
+
+@Component({
+  selector: 'shared-inline-warning',
+  imports: [NgIcon, HlmIcon],
+  providers: [provideIcons({ lucideAlertTriangle })],
+  template: `
+    <ng-icon
+      hlm
+      name="lucideAlertTriangle"
+      [attr.size]="iconSize()"
+      class="text-amber-500"
+      [attr.title]="title() || undefined"
+    />
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class InlineWarningComponent {
+  /** Icon size */
+  iconSize = input<number>(16);
+
+  /** Warning title/tooltip */
+  title = input<string | undefined>();
+}
+
