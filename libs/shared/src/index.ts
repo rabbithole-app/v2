9829a792ce0d16abb4d2afc72f21a33eba2468de
Khalik Faradzhli <khalik.faradzhli@mail.ru>
2025-11-24 11:17:31 +0300

    feat: add snapshot management UI components

--- libs/shared/src/index.ts
+++ libs/shared/src/index.ts
@@ -3,9 +3,12 @@
 
 export * from './lib/components/canister/canister-controllers-table/canister-controllers-table.component';
 export * from './lib/components/canister/canister-health-check/canister-health-check.component';
+export * from './lib/components/canister/canister-load-snapshot-dialog/canister-load-snapshot-dialog.component';
 export * from './lib/components/canister/canister-memory-chart/canister-memory-chart.component';
 export * from './lib/components/canister/canister-memory/canister-memory.component';
 export * from './lib/components/canister/canister-runtime/canister-runtime.component';
+export * from './lib/components/canister/canister-snapshots-table/canister-snapshots-table.component';
+export * from './lib/components/canister/canister-take-snapshot-dialog/canister-take-snapshot-dialog.component';
 export * from './lib/components/canister/commit-permission-warning/commit-permission-warning.component';
 export * from './lib/components/canister/frontend-upload-item/frontend-upload-item.component';
 export * from './lib/components/canister/frontend-upload-list/frontend-upload-list.component';
