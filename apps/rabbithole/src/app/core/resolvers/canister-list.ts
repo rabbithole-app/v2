184a54271859ddfcbe9e23a193d0313b82100aa4
Khalik Faradzhli <khalik.faradzhli@mail.ru>
2025-12-02 12:13:53 +0300

    refactor(resolver): use CanistersService in canister-list resolver

--- apps/rabbithole/src/app/core/resolvers/canister-list.ts
+++ apps/rabbithole/src/app/core/resolvers/canister-list.ts
@@ -1,13 +1,13 @@
+import { inject } from '@angular/core';
 import { ResolveFn } from '@angular/router';
 import { Principal } from '@dfinity/principal';
-import { of } from 'rxjs';
+import { catchError, of } from 'rxjs';
+
+import { CanistersService, resourceToObservable } from '@rabbithole/core';
 
 export const canisterListResolver: ResolveFn<Principal[]> = () => {
-  // Hardcoded list of canisters
-  const canisterIds: Principal[] = [
-    Principal.fromText('uxrrr-q7777-77774-qaaaq-cai'),
-    Principal.fromText('ulvla-h7777-77774-qaacq-cai'),
-  ];
-
-  return of(canisterIds);
+  const canistersService = inject(CanistersService);
+  return resourceToObservable(canistersService.list).pipe(
+    catchError(() => of([])),
+  );
 };
